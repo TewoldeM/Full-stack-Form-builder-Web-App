8de04ee3ef4f31f112a2d884a8ac835c4e49ebab
tewolde marie <tewoldemarie6@gmail.com>
2025-09-16 18:25:05 +0300

    Initial project setup

---
+++ app/(dashboard)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Dasboard = () => {
+  return (
+    <div className='p-16 text-red-500 font-bold text-2'>
+      hello from Dasboard
+    </div>
+  )
+}
+
+export default Dasboard
