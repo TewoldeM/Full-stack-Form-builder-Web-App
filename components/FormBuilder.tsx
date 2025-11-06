d5d3cc332a36993cdacf45eb5218cad19adfe5d1
tewolde marie <tewoldemarie6@gmail.com>
2025-11-06 11:23:21 +0300

    Dnd Sensors are done

--- components/FormBuilder.tsx
+++ components/FormBuilder.tsx
@@ -4,12 +4,15 @@ import React from "react";
 import PreviewDialogBtn from "./PreviewDialogBtn";
 import SaveFormbtn from "./SaveFormbtn";
 import PublishFormbtn from "./PublishFormbtn";
-import { DndContext, DragOverlay } from "@dnd-kit/core";
+import { DndContext, DragOverlay, MouseSensor, TouchSensor, useSensor, useSensors } from "@dnd-kit/core";
 import Designer from "./Designer";
 import DragOverlayWrapper from "./DragOverlayWrapper";
 const FormBuilder = ({ form }: { form: Form }) => {
+  const mouseSensor = useSensor(MouseSensor, { activationConstraint: { distance: 10 } })
+  const touchSensor = useSensor(TouchSensor, { activationConstraint: { delay: 300, tolerance: 5 } })
+  const sensors = useSensors(mouseSensor, touchSensor);
   return (
-    <DndContext>
+    <DndContext sensors={[mouseSensor, touchSensor]}>
       <main className="flex flex-col w-full">
         <nav className="flex jb border-b-2 p-4 gap-3 items-center">
           <h2 className="truncate font-medium">
